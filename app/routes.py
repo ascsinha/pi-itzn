44870f8cf035d56fd580a1b69aafd1246dfea323
Acsa Mirella <mirella.a@escolar.ifrn.edu.br>
2025-11-21 20:24:41 -0300

    Desenvolvendo a página Index

--- app/routes.py
+++ app/routes.py
@@ -4,11 +4,35 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title = 'Index')
+    return render_template('index.html', title = 'Bem-vindo ao ITZN!')
 
-@app.route('/home')
+@app.route('/dashboard')
 def home():
-    return render_template('main/home.html', title = 'Home')
+    return render_template('main/dashboard.html', title = 'Home')
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html', title = 'Log In')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('auth/cadastro.html', title = 'Cadastro')
+
+@app.route('/eventos')
+def eventos():
+    return render_template('main/eventos.html', title = 'Eventos')
+
+@app.route('/editais')
+def editais():
+    return render_template('main/editais.html', title = 'Editais')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('main/sobre.html', title = 'Sobre Nós')
+
+@app.route('/contato')
+def contato():
+    return render_template('main/contato.html', title = 'Contato')
 
 if __name__ == '__main__':
     app.run(debug = True)
