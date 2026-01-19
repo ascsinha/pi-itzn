8219f9951c897701864d4c7be4ee52eaf6fbdf63
Acsa Mirella da Silva Dantas <20231041110004@ifrn.local>
2026-01-19 14:41:41 -0300

    Alterando código de autenticação

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,18 +2,38 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import current_user, login_user, logout_user
 from ..models import Usuario
 import sqlalchemy as sa
-# from flask_wtf import LoginForm, RegistrationForm
+from ..forms.auth import LoginForm, RegistrationForm
 from app import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template('auth/login.html', title = 'Log In')
+    if current_user.is_authenticated:
+        return redirect(url_for('main.agendamentos'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        usuario = Usuario.query.filter_by(email = form.email.data).first()
+        if usuario is None and not usuario.checar_senha(form.senha.data):
+            flash('E-mail ou senha inválido.')
+            return redirect(url_for('auth.login'))
+        login_user(usuario)
+        return redirect(url_for('index_bp.index'))
+    return render_template('auth/login.html', title = 'Log In', form = form)
 
 @auth.route('/cadastro')
 def cadastro():
-    return render_template('auth/cadastro.html', title = 'Cadastro')
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.login'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        usuario =  Usuario(nome = form.nome.data, cpf = form.cpf.data, data_nascimento = form.data_nascimento.data, email = form.email.data, senha = form.senha.data)
+        usuario.criar_senha(form.senha.data)
+        db.session.commit(usuario)
+        db.session.add()
+        flash('Parabéns, você é um usuário cadastrado!')
+        redirect(url_for('auth.login'))
+    return render_template('auth/cadastro.html', title = 'Cadastro', form = form)
 
 @auth.route('/esqueci-senha', methods=['GET', 'POST'])
 def esqueciSenha():
@@ -29,3 +49,8 @@ def esqueciSenha():
         return redirect(url_for('login'))
 
     return render_template('auth/esqueci-senha.html', title='Esqueci a Senha')
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index_bp.index'))
